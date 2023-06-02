9ae3893ae8df93ffb9db932746a2bcbe76a32d86
Sagar Parida <paridag321@gmail.com>
2023-06-02 20:59:54 +0530

    initial setup

---
+++ my-app/src/app/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <div>©2023 Sagar Parida. All Rights Reserved.</div>
+        </div>
+    )
+}
+
+export default Footer
